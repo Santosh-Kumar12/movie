b1c06f31a3ccc61ab8ea660047f7b668c022ab8b
Santoshkumar <santoshpatrimath12@gmail.com>
2024-08-23 13:12:04 +0530

    added basename to Router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-   <BrowserRouter basename='/'>
+   <BrowserRouter  basename='/'>
     <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/movies/:id' element={<SingleMovie/>}/>
