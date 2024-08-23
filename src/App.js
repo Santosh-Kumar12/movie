b4001a3bef71d8261a1aa5378e34900a46c837db
Santoshkumar <santoshpatrimath12@gmail.com>
2024-08-23 13:19:03 +0530

    Updated code with new changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Navbar from './components/Search/Search'
 import Home from './components/Home/Home'
 import Cards from './components/Cards/Cards'
 import SingleMovie from './components/SingleMovie/SingleMovie'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, HashRouter} from 'react-router-dom'
 
 const App = () => {
   return (
-   <BrowserRouter  basename='/'>
+   <HashRouter>
     <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/movies/:id' element={<SingleMovie/>}/>
 
     </Routes>
-   </BrowserRouter>
+   </HashRouter>
   )
 }
 
